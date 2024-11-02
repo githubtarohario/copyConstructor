810b438bd37ce84a8ad1c36fe0c826e77460dd64
githubtarohario <tonkoarimoto@gmail.com>
2024-11-02 12:57:58 +0900

    プロジェクト ファイルを追加します。

---
+++ copyConstructor/copyConstructor.cpp
@@ -0,0 +1,61 @@
+﻿// copyConstructor.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+using namespace std;
+class Dog {
+public:
+    string name;
+    Dog() {
+        cout << "デフォルトコンストラクター" << endl;
+        cout << "Pochi" << endl;
+
+    }
+    Dog(string n) :name(n){
+        cout << "デフォルトコンストラクター" << endl;
+        cout << name << endl;
+
+    }
+
+
+
+    Dog(const Dog& org) {
+        
+        cout << "コピーコンストラクター" << endl;
+        cout << org.name << endl;
+    }
+    ~Dog() {
+        cout << "デストラクター" << endl;
+    }
+};
+void test1(Dog d1) {
+
+
+
+}
+
+void test2(Dog& d) {
+
+
+
+}
+void test3(Dog *d) {
+
+
+
+}
+
+
+
+int main()
+{
+    Dog d("Pochi");
+    //test1(d);
+    //test2(d);
+    //test3(&d);
+    Dog d1 = d;
+
+
+
+}
+
